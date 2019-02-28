5ce91e211630e5f801f22c237669dc5bff4e1cb7
Lillian Dixon <dixon.lillian92@gmail.com>
2019-02-28 08:34:50 -0700

    api work

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,7 @@
-
 import React, { Component } from "react";
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -13,12 +11,19 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    
+  }
+
+
   render() {
     return (
       <div className="app">
         <Router>
           <div>
-            <h1>Jordan Hudgens Portfolio</h1>
+            <h1>Lillian Dixon Portfolio</h1>
             <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationContainer />
 
@@ -27,13 +32,15 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              <Route exact path="/portfolio/:slug" component = {PortfolioDetail}/>
-              <Route component={NoMatch} /> 
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
-
-
       </div>
     );
   }
