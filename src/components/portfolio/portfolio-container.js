2b5720c374f7e95cae7e1cb626478349d05af083
Lillian Dixon <dixon.lillian92@gmail.com>
2019-03-04 09:04:05 -0700

    finished styles

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -4,8 +4,8 @@ import axios from "axios";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       pageTitle: "Welcome to my portfolio",
@@ -25,7 +25,8 @@ export default class PortfolioContainer extends Component {
   }
 
   getPortfolioItems() {
-    axios.get("https://jordan.devcamp.space/portfolio/portfolio_items")
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items")
       .then(response => {
         this.setState({
           data: response.data.portfolio_items
@@ -52,21 +53,18 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div>
-        <h2>{this.state.pageTitle}</h2>
-
-        <button onClick={() => this.handleFilter("eCommerce")}>
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
           eCommerce
         </button>
-        <button onClick={() => this.handleFilter("Scheduling")}>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
           Scheduling
         </button>
-        <button onClick={() => this.handleFilter("Enterprise")}>
+        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
           Enterprise
         </button>
 
-        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
-        
+        {this.portfolioItems()}
       </div>
     );
   }
